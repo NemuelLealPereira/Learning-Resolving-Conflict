26d8c84b47d6c00f1930367750cac0cec058c4cb
nemo <nemuelpereirati@gmail.com>
2015-07-07 13:51:08 -0300

    alterado nome para nomePai

--- Pessoa.java
+++ Pessoa.java
@@ -1,11 +1,11 @@
 public class Pessoa {
 
-	private String nome;
+	private String nomePai;
 	private int idade;
 	private int age;
 
 	public Pessoa(String nomeCompleto, String idade, int age) {
-		this.nome = nomeCompleto;
+		this.nomePai = nomeCompleto;
 		this.idade = idade;
 		this.age = age;
 	}
@@ -15,7 +15,7 @@ public class Pessoa {
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.nomePai = nome;
 	}
 
 	public int getIdade() {
